a926bef0b034f9bf6bd3fdb6ca3251369c371288
Nizar Ahammad <nizartsy007@gmail.com>
2022-08-13 21:30:09 +0530

    driver class

--- Driver/Program.cs
+++ Driver/Program.cs
@@ -10,10 +10,6 @@ namespace Driver
             IChannelFactory.Interfaces.ILogin login = new IChannelFactory.Factory.LoginFactory();
             IChannelFactory.Model.User user = new IChannelFactory.Model.User() { UserID = 123, Password = "123" };
             var res = login.ValidateUser(user);
-            if (res.IsSucess==true)
-            {
-                Console.WriteLine((res.ResponseObject as IChannelFactory.Model.User).UserID.ToString());
-            }
         }
     }
 }
